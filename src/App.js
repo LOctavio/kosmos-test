950105af180e70cbca6cf3542dee81d0e465f624
LOctavio <luis_ram1997@hotmail.com>
2022-05-26 15:46:22 -0500

    Add fields to list and add new state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css';
+import fields from "./data/fields.json";
 import Form from './components/Form';
 import List from './components/List';
 
 function App() {
+  const [fieldsList, setFieldsList] = useState([]);
+
   return (
     <div className='main-container'>
       <Form />
-      <List />
+      <List fields={fields}/>
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,22 @@
-const List = () => {
+import './List.css'
+
+const List = (props) => {
+  const  { fields } = props;
+
   return (
-    <h1>List</h1>
+    <div>
+      <h1>Add fields to form
+      </h1>
+      {fields.map(field => {
+        return(
+          <div key={field.id} className="list">
+            <span>Type: {field.component}</span>
+            <span>Name: {field.name}</span>
+            <button>Add to List</button>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
